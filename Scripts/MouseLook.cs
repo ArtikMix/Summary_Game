6488b5bd75cb9967bcff574a33434077c6e6421f
ArtikMix <a.katkovski5@gmail.com>
2021-09-25 18:33:38 +0300

    human gfx

--- Scripts/MouseLook.cs
+++ Scripts/MouseLook.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    [SerializeField] private Transform player;
-    [SerializeField] private Transform _camera;
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    [SerializeField] private RotationAxes axes = RotationAxes.MouseXAndY;
+    [SerializeField] private float sensitivityHor = 9.0f;
+    [SerializeField] private float sensitivityVert = 9.0f;
+    [SerializeField] private float minimumVert = -45.0f;
+    [SerializeField] private float maximumVert = 45.0f;
+    private float _rotationX = 0;
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.freezeRotation = true;
     }
-
     void Update()
     {
-        float x_Rot = Input.GetAxis("Mouse X") * 2f;
-        float y_Rot = Input.GetAxis("Mouse Y") * 2f;
-        player.Rotate(player.rotation.x, x_Rot, player.rotation.z);
-        _camera.Rotate(x_Rot, 0f, 0f);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2, 0f);
-        Vector3 size = new Vector3(1, 1, 1);
-        Gizmos.DrawCube(center, size);
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
     }
 }
--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -5,18 +5,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float speed = 10f;
-    private float turning = 60f;
+    //private float turning = 60f;
+    [SerializeField] Animator animator;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal") * 6f * Time.deltaTime;
         //transform.Rotate(0, horizontal, 0);
-
+        transform.position = new Vector3(transform.position.x, 7.26f, transform.position.z);
         float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Translate(horizontal, 0, vertical);
+        AnimationCheck(horizontal, vertical);
+    }
+
+    private void AnimationCheck(float hor, float ver)
+    {
+        if (ver > 0 || ver < 0)
+        {
+            animator.SetBool("walking", true);
+        }
+        else if (ver == 0 && hor == 0)
+        {
+            animator.SetBool("walking", false);
+            animator.SetBool("idle", true);
+        }
+        if (hor < 0)
+        {
+            animator.SetBool("left", true);
+        }
+        if (hor > 0)
+        {
+            animator.SetBool("right", true);
+        }
+        if (hor == 0 && ver == 0)
+        {
+            animator.SetBool("left", false);
+            animator.SetBool("right", false);
+            animator.SetBool("idle", true);
+        }
     }
 }
